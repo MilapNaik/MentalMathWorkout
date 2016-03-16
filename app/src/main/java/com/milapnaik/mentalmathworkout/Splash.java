9c618473f02f9793921e0eac04caeb89f7711afa
Milap Naik <milapnaik@gmail.com>
2016-03-15 21:59:42 -0400

    Create and started project
    Added splash screen, main menu, and starting first math quiz. Still
    working on fixing aesthetics.

---
+++ app/src/main/java/com/milapnaik/mentalmathworkout/Splash.java
@@ -0,0 +1,46 @@
+package com.milapnaik.mentalmathworkout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.content.Intent;
+
+/**
+ * Created by MilapNaik on 3/14/16.
+ */
+public class Splash extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final ImageView splash = (ImageView) findViewById(R.id.imageView);
+        final Animation fade = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    //splash.startAnimation(fade);
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
